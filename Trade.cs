e0013b666a52d2deb51b9cd3e88a5f1c1a30cf57
KochetovSerg <8157070@mail.ru>
2025-07-08 17:51:44 +0300

    Add files via upload

---
+++ Trade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3U
+{
+    public class Trade
+    {
+       
+        
+            #region Fields //===========================
+            public int Sec = 0;
+            /// <summary>
+            /// цена сделки
+            /// </summary>
+            public decimal Price = 0;
+            // public decimal Volume  = 0;
+            public string SecCode = "";
+            public string ClassCode = "";
+            public DateTime DateTime = DateTime.Now;
+            /// <summary>
+            /// обьем сделки
+            /// </summary>
+            public decimal Volume
+            {
+                get
+                {
+                    return _volume;
+                }
+                set
+                {
+                    _volume = value;
+                }
+            }
+            decimal _volume = 0;
+            #endregion //==============================
+        
+    }
+}
